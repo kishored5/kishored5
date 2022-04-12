c8cfa306b15ba4ac5c7b7d3842fbaea2ead45d61
kishored5 <99378650+kishored5@users.noreply.github.com>
2022-04-12 12:59:38 +0530

    Add files via upload

---
+++ New_RDP/Agreegatefor sumof numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agreegatefor_sumof_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int sum = array.Aggregate((total, next) => total + next);
+            Console.WriteLine(sum);
+        }
+    }
+}
